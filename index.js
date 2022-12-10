07986b168221aed9ef4830f78819c6c5f58cdaf2
enmalafeev <malafeev.en@yandex.ru>
2022-12-10 16:10:35 +0600

    add runApp function

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log('app is running !!!');
+import { argv } from 'node:process';
+import * as readline from 'node:readline/promises';
+import { stdin as input, stdout as output } from 'process';
+
+const getUserName = (args) => {
+  const firstArg = args[0];
+  return firstArg.split('=')[1];
+};
+
+const runApp = () => {
+  const args = argv.slice(2);
+  const userName = getUserName(args);
+  const greeting = `Welcome to the File Manager, ${userName}!`;
+  const goodbye = `Thank you for using File Manager, ${userName}, goodbye!`
+  console.log(greeting);
+  const rl = readline.createInterface({ input, output });
+  rl.on('SIGINT', () => {
+    console.log(goodbye);
+    rl.close();
+  });
+  rl.on('line', (input) => {
+    if (input === '.exit') {
+      console.log(goodbye);
+      rl.close(); 
+    }
+  });
+};
+
+runApp();
