2f46f21f260fb39b688deb402013e2766bd22606
enmalafeev <malafeev.en@yandex.ru>
2022-12-12 11:23:28 +0600

    add os EOL

--- src/runCommand.js
+++ src/runCommand.js
@@ -1,10 +1,16 @@
 import path from 'path';
+import os from 'os';
 import { state } from './index.js';
 
 const commands = {
   'up': () => { 
     state.cwd = path.resolve(state.cwd, '..');
-  }, 
+  },
+  'os': (arg) => {
+    if (arg === '--EOL') {
+      console.log(JSON.stringify(os.EOL));
+    }
+  }
 }
 
 export default (command) => {
